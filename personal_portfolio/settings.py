342e939027ced49e6e6b7c72d610b84cc508a354
Basil Latif <blatif@ranker.com>
2022-01-23 22:10:49 -0800

    added the url to allowed hosts

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*i@tds1-!a#t!!w&+xzuu-hh0&19+cp2b$+ve56lw!5gt%p$4_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['limitless-peak-38200.herokuapp.com']
+ALLOWED_HOSTS = ['limitless-peak-38200.herokuapp.com', 'www.clickanalytics.live']
 
 
 # Application definition
